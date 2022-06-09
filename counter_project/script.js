ee1d4968781b1d9168f2d85dc74b49e01714e5f9
nandini <nandini.shrivastav_cs20@gla.ac.in>
2022-06-09 19:32:56 +0530

    a simple counter project

---
+++ counter_project/script.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    document.getElementById('value').innerHTML=parseInt(document.getElementById('value').innerHTML)+1;
+}
+function myFunction2(){
+    document.getElementById('value').innerHTML=parseInt(document.getElementById('value').innerHTML)-1;
+}
+function myFunction3(){
+    document.getElementById('value').innerHTML=0;
+}
